1de9d715ffc233d0c6c7547d090f9cde1f94c46b
KaitouKiddo <sc.scott.cai@gmail.com>
2025-04-14 22:26:23 -0400

    feat(footer): improved the footer and fixed the svg file

---
+++ src/consts.ts
@@ -0,0 +1,24 @@
+export const FooterLinks = [
+    { href: "/posts/core-concepts-concepts-why-astro/", title: "Astro" },
+    { href: "/posts/assets-guides-styling/", title: "Styling" },
+    { href: "/tags/", title: "Tags" },
+  ];
+
+export const SocialLinks = [
+{ href: "/rss.xml", icon: "icon-[tabler--rss]", label: "RSS" },
+{
+    href: "https://www.instagram.com/ayakristyart/",
+    icon: "icon-[tabler--brand-instagram]",
+    label: "Instagram",
+},
+{
+    href: "https://www.linkedin.com/in/kristy-xuan-huang/",
+    icon: "icon-[tabler--brand-linkedin]",
+    label: "LinkedIn",
+},
+{
+    href: "https://github.com/kristyayarin/",
+    icon: "icon-[tabler--brand-github]",
+    label: "GitHub",
+},
+];
